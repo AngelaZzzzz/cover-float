ec99689cd1589ae9ca523e0d57da39f38363acc1
Corey Hickson <chickson@hmc.edu>
2025-12-18 01:38:21 -0800

    Add B3 covergroup

---
+++ coverage/covergroups/bins_templates/conversion_op_bins.svh
@@ -0,0 +1,7 @@
+        // all conversion operations
+        bins op_cfi    = {[OP_CFI  : OP_CFI  | 32'hF]};
+        bins op_fcvtw  = {OP_FCVTW};
+        bins op_fcvtwu = {OP_FCVTWU};
+        bins op_fcvtl  = {OP_FCVTL};
+        bins op_fcvtlu = {OP_FCVTLU};
+        bins op_cff    = {[OP_CFF   : OP_CFF   | 32'hF]}; 
