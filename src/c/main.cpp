8a788ee6f5fd5c4552b8eb9a65bef7953295386b
Ryan Wolk <rwolk@hmc.edu>
2026-02-03 17:36:42 -0800

    Cleanup the python extension module and c code

--- src/c/main.cpp
+++ src/c/main.cpp
@@ -3,20 +3,20 @@
 
 namespace py = pybind11;
 
-std::string hello_from_bin() { return "Hello from cover-float!"; }
-
 std::string run_test_vector(const std::string &test_vector, bool suppress_error_check = true) {
-  char* output = nullptr;
+  const size_t OUTPUT_SIZE = 512;
+  char* output = new char[OUTPUT_SIZE];
 
   int res = coverfloat_runtestvector(
       test_vector.c_str(),
       test_vector.size(),
-      &output,
+      output,
+      OUTPUT_SIZE,
       suppress_error_check
   );
 
   std::string result (output);
-  free(output);
+  delete[] output;
 
   if (res != EXIT_SUCCESS) {
       throw py::value_error("Error running test vector: " + result);
@@ -27,10 +27,6 @@ std::string run_test_vector(const std::string &test_vector, bool suppress_error_
 
 PYBIND11_MODULE(_reference, m) {
   m.doc() = "pybind11 hello module";
-
-  m.def("hello_from_bin", &hello_from_bin, R"pbdoc(
-      A function that returns a Hello string.
-  )pbdoc");
 
   m.def("run_test_vector", &run_test_vector, R"pbdoc(
       Run the given vector through the coverfloat reference model.
